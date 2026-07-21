0939d2aab1b50e7eb15a690997fdb8000cdd2473
Maqsudbek <dehkanaliev@gmail.com>
2026-07-21 18:59:13 +0500

    CustomUser and BaseModel models have been created!

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from baseapp.models import BaseModel
 
-# Create your models here.
+
+class CustomUser(AbstractUser, BaseModel):
+    user_role = models.CharField()
+    auth_type = models.CharField()
+    auth_status = models.CharField()
+    email = models.EmailField(unique=True, null=True, blank=True)
+    phone_number = models.CharField(max_length=13, unique=True, null=True, blank=True)
+    photo = models.ImageField(upload_to='users/', blank=True, null=True)
+    
